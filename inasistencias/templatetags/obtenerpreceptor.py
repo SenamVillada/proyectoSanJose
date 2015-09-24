d365f80726c9c031842ca46cc3cea7906204f04f
ezeanello <ezeanello96@gmail.com>
2015-09-24 11:36:49 -0300

    Borramos todo para reorganizar el proyecto

--- inasistencias/templatetags/obtenerpreceptor.py
+++
@@ -1,9 +0,0 @@
-from django import template
-from inasistencias.models import Preceptor
-
-register = template.Library()
-
-@register.simple_tag
-def preceptor(preceptor):
-    preceptor = Preceptor.objects.get(username=preceptor)
-    return preceptor.id
