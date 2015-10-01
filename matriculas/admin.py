eb17d289ac7065e5af37cecfaffe038a784c2fa2
Augusto Jair <augustojair_96@hotmail.com>
2015-10-01 14:55:07 -0300

    creacion clase persona

--- matriculas/admin.py
+++ matriculas/admin.py
@@ -2,10 +2,11 @@ from django.contrib import admin
 from matriculas.models import *
 
 admin.site.register(Alumno)
-admin.site.register(Cargo)
 admin.site.register(Profesor)
+admin.site.register(Cargo)
 admin.site.register(Licencia)
 admin.site.register(Materia)
 admin.site.register(Matricula)
 admin.site.register(Asistencia)
+admin.site.register(Nota)
 admin.site.register(Log)
